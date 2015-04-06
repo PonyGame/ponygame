4a5d73dbb22a289b51c9e1d2fb6d636175451249
unknown <anchu@n103513.merann.ru>
2015-04-06 17:48:15 +0300

    Separate js files created

---
+++ src/main/webapp/js/gameboard.js
@@ -0,0 +1,36 @@
+/* 
+ * This script creates 11x11 game board 
+ * with flipable cards on it
+ */
+
+
+function createTable() {
+    var container = document.getElementById('container');
+
+    for(var i=0;i<11;i++){
+        for (var j=0;j<11;j++) {
+            var cell = document.createElement("div");
+            cell.id = "cell_" + i + "_" + j;
+
+            //boundary conditions
+            if (i==0 || j==0 || i==10 || j==10 
+                || (i==1 && j==1) || (i==1 && j==9) 
+                || (i==9 && j==1) || (i==9 && j==9)) {
+                //invisible divs
+                cell.className='card';
+            }   else { 
+                //flipable cards
+                cell.className = 'card effect__click';
+                var card_front = document.createElement("div");
+                card_front.className = 'card__front';
+                var card_back = document.createElement("div");
+                card_back.className = 'card__back';
+
+                cell.appendChild(card_front);
+                cell.appendChild(card_back);
+            }
+            container.appendChild(cell);
+        }
+    }
+}
+
