8d0e37a446beaf75d8530c52fa252ae28df3af24
silvermilk <silver.milk@yandex.ru>
2015-09-28 21:30:54 +0400

    Added gameboard template

---
+++ src/main/java/com/ponygame/controller/GameboardControllerTemp.java
@@ -0,0 +1,19 @@
+package com.ponygame.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author Nastya
+ */
+
+@Controller
+public class GameboardControllerTemp {
+    
+    @RequestMapping(value = "gameboard")
+    public ModelAndView handleGameboardView() {
+        return new ModelAndView("gameboard");
+    }
+}
---
+++ src/main/webapp/WEB-INF/pages/gameboard.jsp
@@ -0,0 +1,15 @@
+<%@page contentType="text/html" pageEncoding="windows-1251"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+        <title>Gameboard</title>
+    </head>
+    <body>
+        <div class="container">
+            <div id = 'gameboard'></div>
+        </div>
+    </body>
+    
+    <script src="resources/js/gameboard.js"></script>
+</html>
